6da91f828f2fb44cc0cb14b8b954f4f21d0b7b7d
igomes <igomes@rdisoftware.com>
2017-03-27 19:54:33 -0300

    Adding Model with Module entity;

---
+++ BVIAutomation.Model/Entities/Module.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BVIAutomation.Model.Entities
+{
+    public class Module
+    {
+        public int Id { get; set; }
+        public int IdSystem { get; set; }
+        public string FcName { get; set; }
+        public int IdModule { get; set; }
+    }
+}
