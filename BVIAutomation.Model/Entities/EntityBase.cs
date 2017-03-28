03d8cc7d65a57c8302ea7b93fbbe66901d12ee96
igomes <igomes@rdisoftware.com>
2017-03-27 21:30:55 -0300

    Adding Context Base and EntityBase with Module mapping;

---
+++ BVIAutomation.Model/Entities/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BVIAutomation.Model.Entities
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+        public int IdUserInc { get; set; }
+        public int IdUserAlt { get; set; }
+        public DateTime FdInc { get; set; }
+        public DateTime FdAlt { get; set; }
+        public bool FbStatus { get; set; }
+    }
+}
