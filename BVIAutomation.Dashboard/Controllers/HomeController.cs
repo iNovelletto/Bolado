e5a4ca47745c6657540f447bb1def47fa876959d
vfrias <vfrias@rdisoftware.com>
2017-03-27 22:38:41 -0300

    Adding HomeController methods to generate Json tree (not working) and  SystemMappingTests within UnitTest Project

--- BVIAutomation.Dashboard/Controllers/HomeController.cs
+++ BVIAutomation.Dashboard/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using BVIAutomation.Model.Entities;
+using Newtonsoft.Json;
+using System.Web.UI.WebControls;
 
 namespace Bolado.Controllers
 {
@@ -14,5 +15,30 @@ namespace Bolado.Controllers
 
             return View();
         }
+
+        public string GetMap(List<Module> arrModules)
+        {
+            var treeObj = BuildTree(arrModules);
+
+            return JsonConvert.SerializeObject(treeObj);
+        }
+        
+        public TreeNode BuildTree(List<Module> modulesList)
+        {
+            TreeNode rootNode = null;
+            var dic = modulesList.ToDictionary(
+                      module => module.Id,
+                      module => new { Module = module, Node = new TreeNode(module.Id.ToString())}
+                );
+            foreach(var v in dic.Values)
+            {
+                var module = v.Module;
+                if (module.IdModule > 0) dic[(int)module.IdModule].Node.ChildNodes.Add(v.Node);
+                else rootNode = v.Node;
+            }
+
+            return rootNode;
+        }
+        
     }
 }
---
+++ BVIAutomation.Tests/ControllerTests/SystemMappingTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using BVIAutomation.Model.Entities;
+using Bolado.Controllers;
+
+namespace BVIAutomation.Tests
+{
+    [TestClass]
+    public class SystemMappingTests
+    {
+        [TestMethod]
+        public void GetMappingTest()
+        {
+            //This is the populated list
+            var arrModules = new List<Module>();
+
+            //Moque
+            arrModules.Add(new Module { FcName = "A", Id = 1, IdModule = 0, IdSystem = 1 });
+            arrModules.Add(new Module { FcName = "B", Id = 2, IdModule = 0, IdSystem = 1 });
+            arrModules.Add(new Module { FcName = "C", Id = 3, IdModule = 1, IdSystem = 1 });
+
+            var obj = new HomeController();
+
+            var resultJson = obj.GetMap(arrModules);
+
+
+        }
+    }
+}
